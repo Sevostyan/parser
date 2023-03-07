1fc220afff55a4a32e08ee0c436b72c372034c07
Egor Razumilov <razumiloves@gmail.com>
2023-03-07 14:31:19 +0300

    added requirements

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 class PDFparser:
     def __init__(self):
         pass
+
     def parse(self, filename: str):
         pass
+
 
 class HTMLParser:
     def __init__(self):
         pass
+
     def parse(self, raw_text: str):
         pass
+
 
 class DOCXparser:
     def __init__(self):
         pass
+
     def parse(self, filename: str):
         pass
+
 
 class DOCParser:
     def __init__(self):
         pass
+
     def parse(self, filename: str):
         pass
